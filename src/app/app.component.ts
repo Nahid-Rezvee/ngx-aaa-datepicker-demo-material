f6f0730e1fb22135d3801a88d3ac4e9d97804d81
Nahid <abdun.nahid@gmail.com>
2019-04-04 10:57:40 +0600

    project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DatepickerDialogueComponent } from './datepicker-dialogue/datepicker-dialogue.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-aaa-datepicker-demo-material';
+
+  dates: any = [];
+  dateString: string = ''
+
+  constructor(public dialog: MatDialog, private datePipe: DatePipe) { }
+
+  openDatepicker(): void {
+    const dialogRef = this.dialog.open(DatepickerDialogueComponent, {
+      width: '400px'
+    });
+
+    dialogRef.afterClosed().subscribe(data => {
+      this.dateString = '';
+      this.dates = [];
+      if (data && data.hasDatePicked) {
+        if (data.type === 'single' || data.type === 'multi') {
+          this.dates = data.dates;
+        }
+        else {
+          this.dates = data.dates.days;
+        }
+        this.dates.forEach((date) => {
+          this.dateString = this.datePipe.transform(date, 'd MMM y') +', '+this.dateString;
+        });
+      }
+    });
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NgxAaaDatepickerModule } from 'ngx-aaa-datepicker';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { DatepickerDialogueComponent } from './datepicker-dialogue/datepicker-dialogue.component';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatepickerDialogueComponent
+  ],
+  entryComponents: [
+    DatepickerDialogueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxAaaDatepickerModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
